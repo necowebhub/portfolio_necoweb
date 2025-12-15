3dc64ce24bd00ae71eb63480142010ea989a3377
Snorkich <kakashka-x3@yandex.ru>
2025-12-16 00:25:51 +0200

    Fixed import in the index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
+import "./global.css";
 
 createRoot(document.getElementById("root")).render(
     <React.StrictMode>
